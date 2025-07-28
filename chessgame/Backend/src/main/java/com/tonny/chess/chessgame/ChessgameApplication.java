0b6963c33552cbb4511885110f4c917ab334ea89
Tonny Huynh <tonnyhuynh02@gmail.com>
2025-07-28 16:54:09 +0200

    Experimenting

--- chessgame/Backend/src/main/java/com/tonny/chess/chessgame/ChessgameApplication.java
+++ chessgame/Backend/src/main/java/com/tonny/chess/chessgame/ChessgameApplication.java
@@ -2,12 +2,16 @@ package com.tonny.chess.chessgame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ChessgameApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChessgameApplication.class, args);
+		System.out.println("Hi");
 	}
 
 	@GetMapping("/hello")
